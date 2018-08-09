9463db1ef8c783e7b2972061bb415bbbde3c3036
Moon Antonio <pinchodev91@gmail.com>
2018-08-09 05:15:01 +0200

    Agregado nueva version de portfolio

--- portfolio/gulpfile.js
+++ portfolio/gulpfile.js
@@ -1,112 +1,143 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+/**
+ * Gulp file to automate the various tasks
+ */
+
+var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
-var header = require('gulp-header');
-var cleanCSS = require('gulp-clean-css');
-var rename = require("gulp-rename");
+var csscomb = require('gulp-csscomb');
+var cleanCss = require('gulp-clean-css');
+var cache = require('gulp-cache');
+var cssnano = require('gulp-cssnano');
+var del = require('del');
+var imagemin = require('gulp-imagemin');
+var htmlPrettify = require('gulp-html-prettify');
+var gulp = require('gulp');
+var gulpIf = require('gulp-if');
+var gulpRun = require('gulp-run');
+var gulpUtil = require('gulp-util');
+var npmDist = require('gulp-npm-dist');
+var postcss = require('gulp-postcss');
+var runSequence = require('run-sequence');
+var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
-var filter = require('gulp-filter');
-var pkg = require('./package.json');
+var rename = require('gulp-rename');
+var useref = require('gulp-useref-plus');
+var wait = require('gulp-wait');
 
-// Set the banner content
-var banner = ['/*!\n',
-  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-  ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
-  ' */\n',
-  ''
-].join('');
+// Define paths
 
-// Compiles SCSS files from /scss into /css
-gulp.task('sass', function() {
-  return gulp.src('scss/freelancer.scss')
-    .pipe(sass())
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(gulp.dest('css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-});
-
-// Minify compiled CSS
-gulp.task('minify-css', ['sass'], function() {
-  return gulp.src('css/freelancer.css')
-    .pipe(cleanCSS({
-      compatibility: 'ie8'
-    }))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-});
-
-// Minify custom JS
-gulp.task('minify-js', function() {
-  return gulp.src('js/freelancer.js')
-    .pipe(uglify())
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('js'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-});
-
-// Copy vendor files from /node_modules into /vendor
-// NOTE: requires `npm install` before running!
-gulp.task('copy', function() {
-  gulp.src([
-      'node_modules/bootstrap/dist/**/*',
-      '!**/npm.js',
-      '!**/bootstrap-theme.*',
-      '!**/*.map'
-    ])
-    .pipe(gulp.dest('vendor/bootstrap'))
-
-  gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-    .pipe(gulp.dest('vendor/jquery'))
-
-  gulp.src(['node_modules/jquery.easing/*.js'])
-    .pipe(gulp.dest('vendor/jquery-easing'))
-
-  gulp.src([
-      'node_modules/font-awesome/**',
-      '!node_modules/font-awesome/**/*.map',
-      '!node_modules/font-awesome/.npmignore',
-      '!node_modules/font-awesome/*.txt',
-      '!node_modules/font-awesome/*.md',
-      '!node_modules/font-awesome/*.json'
-    ])
-    .pipe(gulp.dest('vendor/font-awesome'))
-})
-
-// Default task
-gulp.task('default', ['sass', 'minify-css', 'minify-js', 'copy']);
-
-// Configure the browserSync task
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: ''
+var paths = {
+    dist: {
+        base: 'dist',
+        img:  'dist/assets/img',
+        libs: 'dist/assets/vendor'
     },
-  })
-})
+    base: {
+        base: './',
+        node: 'node_modules'
+    },
+    src: {
+        base: './',
+        css:  'assets/css',
+        html: '**/*.html',
+        img:  'assets/img/**/*.+(png|jpg|gif|svg)',
+        js:   'assets/js/**/*.js',
+        scss: 'assets/scss/**/*.scss'
+    }
+}
 
-// Dev task with browserSync
-gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'minify-js'], function() {
-  gulp.watch('scss/*.scss', ['sass']);
-  gulp.watch('css/*.css', ['minify-css']);
-  gulp.watch('js/*.js', ['minify-js']);
-  // Reloads the browser whenever HTML or JS files change
-  gulp.watch('*.html', browserSync.reload);
-  gulp.watch('js/**/*.js', browserSync.reload);
+// Compile SCSS
+
+gulp.task('scss', function() {
+  return gulp.src(paths.src.scss)
+    .pipe(wait(500))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([require('postcss-flexbugs-fixes')]))
+    .pipe(autoprefixer({
+        browsers: ['> 1%']
+    }))
+    .pipe(csscomb())
+    .pipe(gulp.dest(paths.src.css))
+    .pipe(browserSync.reload({
+        stream: true
+    }));
+});
+
+// Minify CSS
+
+gulp.task('minify:css', function() {
+  return gulp.src([
+        paths.src.css + '/argon.css'
+    ])
+    .pipe(cleanCss())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(paths.dist.base + '/css'))
+});
+
+// Minify JS
+
+gulp.task('minify:js', function(cb) {
+    return gulp.src([
+            paths.src.base + '/assets/js/argon.js'
+        ])
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(paths.dist.base + '/js'))
+});
+
+// Live reload
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: [paths.src.base, paths.base.base]
+        },
+    })
+});
+
+// Watch for changes
+
+gulp.task('watch', ['browserSync', 'scss'], function() {
+    gulp.watch(paths.src.scss, ['scss']);
+    gulp.watch(paths.src.js, browserSync.reload);
+    gulp.watch(paths.src.html, browserSync.reload);
+});
+
+// Clean
+
+gulp.task('clean:dist', function() {
+    return del.sync(paths.dist.base);
+});
+
+// Copy CSS
+
+gulp.task('copy:css', function() {
+    return gulp.src([
+        paths.src.base + '/assets/css/argon.css'
+    ])
+    .pipe(gulp.dest(paths.dist.base + '/css'))
+});
+
+// Copy JS
+
+gulp.task('copy:js', function() {
+    return gulp.src([
+        paths.src.base + '/assets/js/argon.js'
+    ])
+    .pipe(gulp.dest(paths.dist.base + '/js'))
+});
+
+// Build
+
+gulp.task('build', function(callback) {
+    runSequence('clean:dist', 'scss', 'copy:css', 'copy:js', 'minify:js', 'minify:css',
+        callback);
+});
+
+// Default
+
+gulp.task('default', function(callback) {
+    runSequence(['scss', 'browserSync', 'watch'],
+        callback
+    )
 });
