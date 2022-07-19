8956e430dcb11b81877db4310f83edfe189eaa23
MoonAntonio <pinchodev91@gmail.com>
2022-07-19 23:40:15 +0200

    Agregado modo oscuro

---
+++ js/darkmode.js
@@ -0,0 +1,26 @@
+var toggle = document.getElementById("dark-mode-toggle");
+var darkTheme = document.getElementById("dark-mode-theme");
+var savedTheme = localStorage.getItem("dark-mode-storage") || "light";
+setTheme(savedTheme);
+
+toggle.addEventListener("click", () => {
+    if (toggle.className === "fa fa-moon-o") {
+      setTheme("dark");
+      localStorage.setItem("dark-mode-storage", "dark");
+    } else if (toggle.className === "fa fa-sun-o") {
+      setTheme("light");
+      localStorage.setItem("dark-mode-storage", "light");
+    }
+});
+
+function setTheme(mode) {
+    if (mode === "dark") {
+        darkTheme.disabled = false;
+        toggle.className = "fa fa-sun-o";
+        console.log("Dark OFF");
+    } else if (mode === "light") {
+        darkTheme.disabled = true;
+        toggle.className = "fa fa-moon-o";
+        console.log("Dark ON");
+    }
+}
